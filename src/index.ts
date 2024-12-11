4a4f8bc1e67e47756cb991818e04f58e1eefe3b3
Harshil Agrawal <harshil@cloudflare.com>
2024-12-11 23:41:46 +0530

    add bindings

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,7 @@ export default {
 		if (pathname === '/api/clickstream' && method === 'POST') {
 			const body = (await request.json()) as { data: any };
 			try {
-				const response = await fetch(`https://${env.PIPELINES_ID}.pipelines.cloudflare.com`, {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify([body.data]),
-				});
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
+				await env.MY_PIPELINE.send([body.data]);
 				return new Response('OK', { status: 200 });
 			} catch (error) {
 				console.error(error);
